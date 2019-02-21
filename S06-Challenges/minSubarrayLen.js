2afa590871687fef9fa306f23593556f7b2b7c75
Michael Kardjian <makardjian@gmail.com>
2019-02-21 14:10:38 -0800

    solution for minSubArrayLen

--- S06-Challenges/minSubarrayLen.js
+++ S06-Challenges/minSubarrayLen.js
@@ -6,15 +6,39 @@
  * greater than or equal to the integer passed to the function. If ther isn't one, return 0 instead.
  */
 
+ const minSubArrayLen = (arr, num) => {
+   let start = 0; 
+   let end = 0;
+   let overallMinLen = Infinity;
+
+   while (end < arr.length) {
+     let tempSum = 0;
+     let tempLength = 0;
+     for (let i = start; i <= end; i += 1) {
+       tempSum += arr[i];
+     }
+
+     if (tempSum >= num) {
+       tempLength = end - start + 1;
+       overallMinLen = Math.min(tempLength, overallMinLen);
+       start += 1;
+     } else {
+       end += 1;
+     }
+   }
+   return overallMinLen === Infinity ? 0 : overallMinLen;
+ }
+
 /**
- * I: an array of positive integers and another positive integer
- * O: a number representing the minimal length of a subarray >= the second param
- * C: none
- * E: return zero if no solution exists
- *  /since there are only positive integers, if the length of the array is < num, return 0
- *
- * Brainstorm: 
- *  
- * [1, 2, 3, 2, 2, 4, 5, 8], 10
+ * Example Input: [3, 4, 6, 3, 1, 4, 6, 10, 3, 5], 17
  * 
+ *  Valid subArrays that this algo will identify:
+ *  [3, 4, 6, 3, 1]
+ *  [4, 6, 3, 1, 4]
+ *  [6, 3, 1, 4, 6]
+ *  [3, 1, 4, 6, 10]
+ *  [1, 4, 6, 10]
+ *  [4, 6, 10]
+ *  [6, 10, 3]
+ *  [10, 3, 5]
  */
