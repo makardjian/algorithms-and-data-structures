55be2b25f6dca9a29d1d4a2932e5c39ad443ef3d
Michael Kardjian <makardjian@gmail.com>
2019-02-26 12:09:17 -0800

    complete S22-binary-search-trees

---
+++ S22-Binary-search-trees/binarySearchTree.js
@@ -0,0 +1,58 @@
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor () {
+    this.root = null;
+  }
+
+  // O(log(n)) but not guaranteed
+  insert(value) {
+    let newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    let prev;
+    
+    while (current !== null) {
+      if (current.value === newNode.value) return false;
+      if (newNode.value > current.value) {
+        prev = current;
+        current = current.right
+      } else {
+        prev = current;
+        current = current.left;
+      }
+    }
+
+    if (newNode.value > prev.value) {
+      prev.right = newNode;
+    } else {
+      prev.left = newNode;
+    }
+    return this;
+  }
+
+  // O(log(n)) but not guaranteed
+  contains(value) {
+    if (!this.root) return false;
+    let current = this.root;
+    while (current !== null) {
+      if (value === current.value) return true;
+      if (value < current.value) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+    return false;
+  }
+}
