c722543e50b019975b192136e45e522b81a1aa8b
Michael Kardjian <makardjian@gmail.com>
2019-03-04 15:33:52 -0800

    complete doublyLinkedList.js

--- S20-Doubly-linked-lists/doublyLinkedList.js
+++ S20-Doubly-linked-lists/doublyLinkedList.js
@@ -14,7 +14,7 @@ class DoublyLinkedList {
     this.length = 0;
   }
 
-  
+  // O(1);
   push(val) {
     let newNode = new Node(val);
     if (!this.length) {
@@ -29,6 +29,7 @@ class DoublyLinkedList {
     return this;
   }
   
+  // O(1);
   pop() {
     if (!this.length) return undefined;
     let removed;
@@ -47,6 +48,7 @@ class DoublyLinkedList {
     return removed;
   }
 
+  // O(1);
   shift() {
     if (!this.length) return undefined;
     let removed;
@@ -64,6 +66,7 @@ class DoublyLinkedList {
     return removed;
   }
 
+  // O(1);
   unshift(val) {
     let newNode = new Node(val);
     if (this.length === 0) {
@@ -78,12 +81,69 @@ class DoublyLinkedList {
     return this;
   }
 
+  // O(n / 2) which simplifies to O(n) or linear time complexity;
   get(index) {
-    
+    if (index < 0 || index >= this.length) return undefined;
+    let midPoint = Math.floor(this.length / 2);
+    let counter, current;
+    if (index < midPoint) {
+      counter = 0;
+      current = this.head;
+      while (counter < index) {
+        counter += 1; 
+        current = current.next;
+      }
+    } else {
+      counter = this.length - 1;
+      current = this.tail;
+      while (counter > index) {
+        counter -= 1;
+        current = current.prev;
+      }
+    } 
+    return current;
   }
 
+  // O(n) because it invokes this.get();
+  set(index, newVal) {
+    let node = this.get(index);
+    if (node === undefined) return false;
+    node.val = newVal;
+    return true;
+  }
 
+  //O(1) once you have access to the correct point of insertion
+  //Altogether, O(n) since this.get() is invoked the majority of the time.
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return !!this.push(val);
+    if (index === 0) return !!this.unshift(val);
+    let newNode = new Node(val);
+    let nodeAfter = this.get(index);
+    let nodeBefore = nodeAfter.prev;
+    newNode.prev = nodeBefore;
+    newNode.next = nodeAfter;
+    nodeBefore.next = newNode;
+    nodeAfter.prev = newNode;
+    this.length += 1;
+    return true;
+  }
 
-
-
+  //O(1) once you have access to the correct point of insertion
+  //Altogether, O(n) since this.get() is invoked the majority of the time.
+  remove(index) {
+    if (!this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    let target = this.get(index);
+    if (target === undefined) return undefined;
+    let nodeBefore = target.prev;
+    let nodeAfter = target.next;
+    nodeBefore.next = nodeAfter;
+    nodeAfter.prev = nodeBefore;
+    target.next = null;
+    target.prev = null;
+    this.length -= 1; 
+    return target;
+  }
 }
